9f502bb6cb4c7aa66eff077f3835c3fbd3487306
Axier Anderzon Perlaza Melendez <axierperlaz2018@gmail.com>
2025-01-29 21:13:47 -0500

    add route for add comment in post

--- src/principal/routes/principal.route.ts
+++ src/principal/routes/principal.route.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
 import { getPosts } from "../queries/select.query";
+import { jwt } from "hono/jwt";
+import { JWT_SECRET } from "../../utils/config.utils";
+
 
 export const principalRouter = new Hono();
 
+// final path -> /api/v1/principal/
 principalRouter.get('/', async (c)=>{
     const posts = await getPosts()
     return c.json({posts})
 })
+
+principalRouter.post(
+    '/commented',
+    jwt({
+        secret: JWT_SECRET
+    }),
+    async (c)=>{
+        const payload = c.get('jwtPayload');
+        return c.json({message: "You can realize a comment in post", payload});
+    }
+)
